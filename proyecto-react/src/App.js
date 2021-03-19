b9ad2aa0c0dbd43ff0ca4bfbcd51424b4c09fd11
lauti <ortizl@udesa.edu.ar>
2021-03-19 04:02:04 -0300

    nuevo estilo

--- proyecto-react/src/App.js
+++ proyecto-react/src/App.js
@@ -1,8 +1,11 @@
+// import Items from "./components/items"
+
 
 function App() {
   return (
     <div className="App">
-      <h1>prueba de react</h1>
+      {/* <h1>prueba de react</h1> */}
+     
     </div>
   );
 }
---
+++ proyecto-react/src/components/items.js
@@ -0,0 +1,14 @@
+// nombrar componentes con mayuscula
+
+// import React from 'react';
+
+// function Items(){
+//     return(
+//         <li><p>aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa</p></li>;
+        
+
+//     );
+// }
+
+// export default Items; por si quiero exportarlo
+
