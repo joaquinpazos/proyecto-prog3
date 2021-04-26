fb88d85cd2754f39104abcdba9d82431da81b8e3
lauti <ortizl@udesa.edu.ar>
2021-04-26 19:57:15 -0300

    ver tarjetas components

---
+++ proyecto-react/src/components/tarjeta.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import react from 'react';
+
+
+function tarjetas() {
+	return (
+		<React.Fragment>
+            <div class="center">
+
+                <div class="card"></div>
+                    <div class="general">
+                     <h1>Lautaro Ortiz</h1>
+                     <p><b>E-Mail:</b> lautarortiz@mail.com</p>
+                     <p><b>Cumpleaños:</b> 12/04/1999 (22 años)</p>
+        </React.Fragment>
+		);
+}
+
+
+
