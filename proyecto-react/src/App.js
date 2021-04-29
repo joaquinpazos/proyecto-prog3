0b58745cee2819cd2abcf0a88ad3ec5b230151b5
joacopazos <jpazospesquero@udesa.edu.ar>
2021-04-29 15:34:35 -0300

    comentados

--- proyecto-react/src/App.js
+++ proyecto-react/src/App.js
@@ -2,7 +2,7 @@ import React, {Component} from 'react';
 import TarjetaContenedor from "./components/TarjetaContenedor"
 
 
-class Tarjeta extends Component {
+export default class Tarjeta extends Component {
 
   constructor(props){
     super(props);
@@ -13,34 +13,43 @@ class Tarjeta extends Component {
 
   componentDidMount(){
 
-    fetch('https://randomuser.me/api/?results=10&inc=name,email,picture,dob')
+    fetch('https://randomuser.me/api/?results=20')
     .then(result => result.json())
     .then(data => {
-      console.log(data.results);
+      this.setState({items: data.results});
     })
 
+    .catch((info)=> console.log(info));
   }
   render(){
     return(
-      <div>
+      <div className= "body">
+        <header className="header">
+
+        </header>
+        <div className="addTarjetas">
+            
+        </div>
+        <div className="filtros">
+
+        </div>
         <TarjetaContenedor/>
+        {/* <div className="tarjetas">
+        {this.setState.items.map((info)=>{
+          return  <TarjetaContenedor
+          key= {info.id}
+          informacion= {info}
+          /> })
+          }
+        </div> */}
+        <div className="reset">
+
+        </div>
+        <footer className="footer">
+
+        </footer>
       </div>
     )
   }
 }
-/* function App() {
-  return (
 
-       
-         <div>
-            
-            <TarjetaContenedor/>  
-
-         </div>
-        
-      
-
-  );
-} */
-
-export default Tarjeta;
