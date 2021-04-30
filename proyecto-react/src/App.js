cd99fc462eb7f025686d672b2ae6c4e4bb8c5bca
Valentina Nies <valunies@gmail.com>
2021-04-29 22:49:35 -0300

    arreglo app.js

--- proyecto-react/src/App.js
+++ proyecto-react/src/App.js
@@ -1,56 +1,14 @@
-import React, {Component} from 'react';
+import react from 'react';
 import TarjetaContenedor from "./components/TarjetaContenedor"
 
 
-export default class App extends Component {
-
-  constructor(props){
-    super(props);
-    this.state= {
-      items:[],
-    }
-  }
-
-  componentDidMount(){
-
-    fetch('https://randomuser.me/api/?results=10')
-    .then(result => result.json())
-    .then(data => {
-      this.setState({items: data.results});
-      console.log(data.results)
-    })
-   
-  }
-  render(){
-
-    return(
-      <div className= "body">
-        <header className="header">
-
-        </header>
-        <div className="addTarjetas">
-            
-        </div>
-        <div className="filtros">
-
-        </div>
+function App (){
+  return(
+    <div>
         <TarjetaContenedor/>
-        {/* <div className="tarjetas">
-        {this.setState.items.map((info)=>{
-          return  <TarjetaContenedor
-          key= {info.id}
-          informacion= {info}
-          /> })
-          }
-        </div> */}
-        <div className="reset">
-
-        </div>
-        <footer className="footer">
-
-        </footer>
-      </div>
-    )
-  }
+    </div>
+  );
 }
+
+export default App;
 
